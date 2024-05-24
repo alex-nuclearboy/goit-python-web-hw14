2255f067dd599e2fcbed8f2094c6d64d6ff864ae
Aleksander <alex.nuclearboy@gmail.com>
2024-05-24 16:17:11 +0200

    Fix documentation

--- src/repository/users.py
+++ src/repository/users.py
@@ -9,13 +9,14 @@ SQLAlchemy sessions for database interactions, ensuring transactions are
 handled efficiently and safely.
 
 Functions:
-- get_user_by_email: Fetch a user from the database by their email address.
-- create_user: Create a new user in the database and optionally assign
-               a Gravatar image as the avatar.
-- update_token: Update or clear the refresh token associated with a user.
-- confirm_email: Confirm a user's email address and update the database
-                 to reflect this change.
-- update_avatar: Update a user's avatar image URL in the database.
+  - ``get_user_by_email``: Fetch a user from the database by their email \
+                           address.
+  - ``create_user``: Create a new user in the database and optionally assign \
+                     a Gravatar image as the avatar.
+  - ``update_token``: Update or clear the refresh token associated with a user.
+  - ``confirm_email``: Confirm a user's email address and update the database \
+                       to reflect this change.
+  - ``update_avatar``: Update a user's avatar image URL in the database.
 
 The operations are designed to be used with FastAPI as dependencies within
 route handlers, allowing for seamless integration into web applications
@@ -33,12 +34,12 @@ async def get_user_by_email(email: str, db: Session) -> User:
     """
     Retrieve a user by their email address from the database.
 
-    Args:
-        email (str): The email address of the user to retrieve.
-        db (Session): The SQLAlchemy session for database interaction.
-
-    Returns:
-        User: The user object if found, otherwise None.
+    :param email: The email address of the user to retrieve.
+    :type email: str
+    :param db: The SQLAlchemy session for database interaction.
+    :type db: Session
+    :return: The user object if found, otherwise None.
+    :rtype: User
     """
     return db.query(User).filter(User.email == email).first()
 
@@ -47,13 +48,12 @@ async def create_user(body: UserModel, db: Session) -> User:
     """
     Create a new user in the database and fetch a Gravatar image if available.
 
-    Args:
-        body (UserModel): The user data transfer object containing
-                          user details.
-        db (Session): The SQLAlchemy session for database interaction.
-
-    Returns:
-        User: The newly created user object with all details.
+    :param body: The user data transfer object containing user details.
+    :type body: UserModel
+    :param db: The SQLAlchemy session for database interaction.
+    :type db: Session
+    :return: The newly created user object with all details.
+    :rtype: User
     """
     avatar = None
     try:
@@ -72,11 +72,15 @@ async def update_token(user: User, token: str | None, db: Session) -> None:
     """
     Update the refresh token for a user in the database.
 
-    Args:
-        user (User): The user object whose token is to be updated.
-        token (str | None): The new refresh token to be saved.
-                            If None, it will clear the existing token.
-        db (Session): The SQLAlchemy session for database interaction.
+    :param user: The user object whose token is to be updated.
+    :type user: User
+    :param token: The new refresh token to be saved. \
+                  If None, it will clear the existing token.
+    :type token: str | None
+    :param db: The SQLAlchemy session for database interaction.
+    :type db: Session
+    :return: None
+    :rtype: None
     """
     user.refresh_token = token
     db.commit()
@@ -88,10 +92,13 @@ async def confirm_email(email: str, db: Session) -> None:
     'confirmed' status of the user to True, indicating that the user has
     verified their email address.
 
-    Args:
-        email (str): The email address of the user whose email confirmation
-                     status needs to be updated.
-        db (Session): The SQLAlchemy session for database interaction.
+    :param email: The email address of the user whose email confirmation \
+                  status needs to be updated.
+    :type email: str
+    :param db: The SQLAlchemy session for database interaction.
+    :type db: Session
+    :return: None
+    :rtype: None
     """
     user = await get_user_by_email(email, db)
     user.confirmed = True
@@ -104,14 +111,14 @@ async def update_avatar(email, url: str, db: Session) -> User:
     sets the 'avatar' field of the user to the new URL provided, which points
     to the location of the updated image.
 
-    Args:
-        email (str): The email address of the user whose avatar
-                     is being updated.
-        url (str): The new URL pointing to the avatar image.
-        db (Session): The SQLAlchemy session for database interaction.
-
-    Returns:
-        User: The updated user object after changing the avatar.
+    :param email: The email address of the user whose avatar is being updated.
+    :type email: str
+    :param url: The new URL pointing to the avatar image.
+    :type url: str
+    :param db: The SQLAlchemy session for database interaction.
+    :type db: Session
+    :return: The updated user object after changing the avatar.
+    :rtype: User
     """
     user = await get_user_by_email(email, db)
     user.avatar = url
