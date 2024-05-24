6b9a0c910a1159829b740025a2e2ada5ddda8e52
Aleksander <alex.nuclearboy@gmail.com>
2024-05-24 12:03:39 +0200

    Add comments

--- migrations/env.py
+++ migrations/env.py
@@ -21,9 +21,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-#target_metadata = None
+# target_metadata = None
 target_metadata = Base.metadata
 
+# replace the database connection string
 config.set_main_option("sqlalchemy.url", SQLALCHEMY_DATABASE_URL)
 
 # other values from the config, defined by the needs of env.py,
