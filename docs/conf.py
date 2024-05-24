0e6a0f3a387ed603dfa265746951702831e13404
Aleksander <alex.nuclearboy@gmail.com>
2024-05-24 20:25:17 +0200

    Update configuration settings
    Added an absolute path to the parent directory
    Specified a file with environment variables
    Added an extension
    Changed the theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,20 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Define the project root path
+project_root = Path('..')
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path=project_root / '.env')
+
+# Add the project directory to the sys.path
+sys.path.append(os.path.abspath(project_root))
+
 project = 'Contact Management API'
 copyright = '2024, Alex'
 author = 'Alex'
@@ -13,15 +27,14 @@ author = 'Alex'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'nature'
 html_static_path = ['_static']
