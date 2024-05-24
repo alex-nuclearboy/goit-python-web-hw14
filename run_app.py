b4bc64cfd99a41254cecb885c984e69a0313f60a
Aleksander <alex.nuclearboy@gmail.com>
2024-05-24 11:27:58 +0200

    Add description

--- run_app.py
+++ run_app.py
@@ -1,3 +1,8 @@
+"""
+This script starts the FastAPI application using Uvicorn as the ASGI server.
+It is configured to run on localhost at port 8000 and will automatically
+reload during development if any changes are detected in the codebase.
+"""
 import uvicorn
 
 if __name__ == "__main__":
