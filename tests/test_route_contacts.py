3a98b5c56f897422a34dd11d949e3deb4389feb7
Aleksander <alex.nuclearboy@gmail.com>
2024-06-02 17:42:09 +0200

    Add a fixture that simulates authentication
    Returns an access_token for authorisation

---
+++ tests/test_route_contacts.py
@@ -0,0 +1,23 @@
+from unittest.mock import MagicMock
+
+import pytest
+
+from src.database.models import User
+
+
+@pytest.fixture()
+def token(client, user, session, monkeypatch):
+    mock_send_email = MagicMock()
+    monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
+    client.post("/api/auth/signup", json=user)
+    current_user: User = (
+        session.query(User).filter(User.email == user.get('email')).first()
+    )
+    current_user.confirmed = True
+    session.commit()
+    response = client.post(
+        "/api/auth/login",
+        data={"username": user.get('email'), "password": user.get('password')},
+    )
+    data = response.json()
+    return data["access_token"]
