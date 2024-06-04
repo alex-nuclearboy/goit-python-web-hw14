c47b4480bbfcdf928ad8b5f926b9e734d3f3b271
Aleksander <alex.nuclearboy@gmail.com>
2024-06-04 12:23:50 +0200

    Add a mock for limits

--- tests/test_route_contacts.py
+++ tests/test_route_contacts.py
@@ -1,12 +1,14 @@
 from unittest.mock import MagicMock, patch
 
-import pytest
+from pytest import mark, fixture
+
+from fastapi import status
 
 from src.database.models import User
 from src.services.auth import auth_service
 
 
-@pytest.fixture()
+@fixture()
 def token(client, user, session, monkeypatch):
     mock_send_email = MagicMock()
     monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
@@ -24,6 +26,7 @@ def token(client, user, session, monkeypatch):
     return data["access_token"]
 
 
+@mark.usefixtures('mock_rate_limit')
 def test_create_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
@@ -37,7 +40,7 @@ def test_create_contact(client, token):
                   "additional_info": "Test"},
             headers={"Authorization": f"Bearer {token}"}
         )
-        assert response.status_code == 201, response.text
+        assert response.status_code == status.HTTP_201_CREATED, response.text
         data = response.json()
         assert data["first_name"] == "John"
         assert data["last_name"] == "Doe"
@@ -45,4 +48,5 @@ def test_create_contact(client, token):
         assert data["phone_number"] == "380504238517"
         assert data["birthday"] == "2000-01-01"
         assert data["additional_info"] == "Test"
+        assert data["id"] == 1
         assert "id" in data
