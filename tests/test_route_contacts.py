7575c9044d666096acbc9c0d17f0153eba9d16cf
Aleksander <alex.nuclearboy@gmail.com>
2024-06-04 11:44:15 +0200

    Add testing of the contact route

--- tests/test_route_contacts.py
+++ tests/test_route_contacts.py
@@ -1,8 +1,9 @@
-from unittest.mock import MagicMock
+from unittest.mock import MagicMock, patch
 
 import pytest
 
 from src.database.models import User
+from src.services.auth import auth_service
 
 
 @pytest.fixture()
@@ -21,3 +22,27 @@ def token(client, user, session, monkeypatch):
     )
     data = response.json()
     return data["access_token"]
+
+
+def test_create_contact(client, token):
+    with patch.object(auth_service, 'r') as r_mock:
+        r_mock.get.return_value = None
+        response = client.post(
+            "/api/contacts",
+            json={"first_name": "John",
+                  "last_name": "Doe",
+                  "email": "john.doe@example.com",
+                  "phone_number": "380504238517",
+                  "birthday": "2000-01-01",
+                  "additional_info": "Test"},
+            headers={"Authorization": f"Bearer {token}"}
+        )
+        assert response.status_code == 201, response.text
+        data = response.json()
+        assert data["first_name"] == "John"
+        assert data["last_name"] == "Doe"
+        assert data["email"] == "john.doe@example.com"
+        assert data["phone_number"] == "380504238517"
+        assert data["birthday"] == "2000-01-01"
+        assert data["additional_info"] == "Test"
+        assert "id" in data
