f7812537954c1e84ec2a5ae71f040aa891dc67cd
Aleksander <alex.nuclearboy@gmail.com>
2024-06-05 17:31:27 +0200

    Add documentation

--- tests/test_route_auth.py
+++ tests/test_route_auth.py
@@ -1,9 +1,50 @@
+"""
+Testing Module for User Authentication Operations
+-------------------------------------------------
+
+This module provides a suite of synchronous test cases for testing the user
+authentication operations of an API using FastAPI and SQLAlchemy. These tests
+aim to verify the correct behavior of the API's interaction with a PostgreSQL
+database, focusing on user registration, login, and authentication.
+
+The tests use the pytest framework and MagicMock to simulate database
+operations and interactions.
+
+**Functions**:
+
+- ``test_create_user``: Verifies user creation and email sending functionality.
+- ``test_repeat_create_user``: Ensures proper handling of duplicate user \
+                               registration attempts.
+- ``test_login_user_not_confirmed``: Checks the behavior when a user attempts \
+                                     to log in without email confirmation.
+- ``test_login_user``: Tests successful user login after email confirmation.
+- ``test_login_wrong_password``: Verifies the login process when an incorrect \
+                                 password is provided.
+- ``test_login_wrong_email``: Checks the login process when an invalid email \
+                              is provided.
+
+**Usage**:
+
+- The test suite can be executed as a standalone script by running the module:
+
+.. code-block:: python
+
+    pytest tests/test_route_auth.py -v
+"""
+
 from unittest.mock import MagicMock
 
 from src.database.models import User
 
 
 def test_create_user(client, user, monkeypatch):
+    """
+    Verifies user creation and email sending functionality.
+
+    :param client: The test client for making HTTP requests.
+    :param user: A dictionary containing user details.
+    :param monkeypatch: pytest's monkeypatch fixture for modifying behavior.
+    """
     mock_send_email = MagicMock()
     monkeypatch.setattr("src.routes.auth.send_email", mock_send_email)
     response = client.post(
@@ -17,6 +58,9 @@ def test_create_user(client, user, monkeypatch):
 
 
 def test_repeat_create_user(client, user):
+    """
+    Ensures proper handling of duplicate user registration attempts.
+    """
     response = client.post(
         "/api/auth/signup",
         json=user,
@@ -27,6 +71,10 @@ def test_repeat_create_user(client, user):
 
 
 def test_login_user_not_confirmed(client, user):
+    """
+    Checks the behavior when a user attempts to log in without \
+    email confirmation.
+    """
     response = client.post(
         "/api/auth/login",
         data={"username": user.get('email'), "password": user.get('password')},
@@ -37,6 +85,9 @@ def test_login_user_not_confirmed(client, user):
 
 
 def test_login_user(client, session, user):
+    """
+    Tests successful user login after email confirmation.
+    """
     current_user: User = (
         session.query(User).filter(User.email == user.get('email')).first()
     )
@@ -52,6 +103,9 @@ def test_login_user(client, session, user):
 
 
 def test_login_wrong_password(client, user):
+    """
+    Verifies the login process when an incorrect password is provided.
+    """
     response = client.post(
         "/api/auth/login",
         data={"username": user.get('email'), "password": 'password'},
@@ -62,6 +116,9 @@ def test_login_wrong_password(client, user):
 
 
 def test_login_wrong_email(client, user):
+    """
+    Checks the login process when an invalid email is provided.
+    """
     response = client.post(
         "/api/auth/login",
         data={"username": 'email', "password": user.get('password')},
