5962bcbddcebc53c2502a1d6023ba6a785f69c0a
Aleksander <alex.nuclearboy@gmail.com>
2024-06-05 17:12:10 +0200

    Automatic setup of birthday

--- tests/test_route_contacts.py
+++ tests/test_route_contacts.py
@@ -7,6 +7,8 @@ from fastapi import status
 from src.database.models import User
 from src.services.auth import auth_service
 
+from datetime import datetime, timedelta
+
 
 @fixture()
 def token(client, user, session, monkeypatch):
@@ -30,13 +32,16 @@ def token(client, user, session, monkeypatch):
 def test_create_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
+        tmp_day = datetime.now() + timedelta(days=3)  # Current date + 3 days
+        birthday = tmp_day.replace(year=2000)  # Change the year to 2000
+        birthday_str = birthday.strftime("%Y-%m-%d")
         response = client.post(
             "/api/contacts",
             json={"first_name": "John",
                   "last_name": "Doe",
                   "email": "john.doe@example.com",
                   "phone_number": "380504238517",
-                  "birthday": "2000-01-01",
+                  "birthday": birthday_str,
                   "additional_info": "Test"},
             headers={"Authorization": f"Bearer {token}"}
         )
@@ -46,7 +51,7 @@ def test_create_contact(client, token):
         assert data["last_name"] == "Doe"
         assert data["email"] == "john.doe@example.com"
         assert data["phone_number"] == "380504238517"
-        assert data["birthday"] == "2000-01-01"
+        assert data["birthday"] == birthday_str
         assert data["additional_info"] == "Test"
         assert data["id"] == 1
         assert "id" in data
@@ -56,6 +61,9 @@ def test_create_contact(client, token):
 def test_get_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
+        tmp_day = datetime.now() + timedelta(days=3)  # Current date + 3 days
+        birthday = tmp_day.replace(year=2000)  # Change the year to 2000
+        birthday_str = birthday.strftime("%Y-%m-%d")
         response = client.get(
             "/api/contacts/1",
             headers={"Authorization": f"Bearer {token}"}
@@ -66,7 +74,7 @@ def test_get_contact(client, token):
         assert data["last_name"] == "Doe"
         assert data["email"] == "john.doe@example.com"
         assert data["phone_number"] == "380504238517"
-        assert data["birthday"] == "2000-01-01"
+        assert data["birthday"] == birthday_str
         assert data["additional_info"] == "Test"
         assert "id" in data
 
@@ -99,6 +107,9 @@ def test_get_contact_not_found(client, token):
 def test_get_contacts(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
+        tmp_day = datetime.now() + timedelta(days=3)  # Current date + 3 days
+        birthday = tmp_day.replace(year=2000)  # Change the year to 2000
+        birthday_str = birthday.strftime("%Y-%m-%d")
         response = client.get(
             "/api/contacts",
             headers={"Authorization": f"Bearer {token}"}
@@ -110,7 +121,7 @@ def test_get_contacts(client, token):
         assert data[0]["last_name"] == "Doe"
         assert data[0]["email"] == "john.doe@example.com"
         assert data[0]["phone_number"] == "380504238517"
-        assert data[0]["birthday"] == "2000-01-01"
+        assert data[0]["birthday"] == birthday_str
         assert data[0]["additional_info"] == "Test"
         assert "id" in data[0]
 
@@ -130,18 +141,21 @@ def test_get_contacts_unauthorised(client):
 def test_update_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
+        tmp_day = datetime.now() + timedelta(days=3)  # Current date + 3 days
+        birthday = tmp_day.replace(year=1990)  # Change the year to 1990
+        birthday_str = birthday.strftime("%Y-%m-%d")
         response = client.patch(
             "/api/contacts/1",
             json={"first_name": "Mike",
                   "email": "mike.doe@example.com",
-                  "birthday": "1999-12-31",
+                  "birthday": birthday_str,
                   "additional_info": "Update test"},
             headers={"Authorization": f"Bearer {token}"}, )
         assert response.status_code == status.HTTP_200_OK, response.text
         data = response.json()
         assert data["first_name"] == "Mike"
         assert data["email"] == "mike.doe@example.com"
-        assert data["birthday"] == "1999-12-31"
+        assert data["birthday"] == birthday_str
         assert data["additional_info"] == "Update test"
         assert "id" in data
 
@@ -150,11 +164,14 @@ def test_update_contact(client, token):
 def test_update_contact_not_found(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
+        tmp_day = datetime.now() + timedelta(days=3)  # Current date + 3 days
+        birthday = tmp_day.replace(year=1990)  # Change the year to 1990
+        birthday_str = birthday.strftime("%Y-%m-%d")
         response = client.patch(
             "/api/contacts/10",
             json={"first_name": "Mike",
                   "email": "mike.doe@example.com",
-                  "birthday": "1999-12-31",
+                  "birthday": birthday_str,
                   "additional_info": "Update test"},
             headers={"Authorization": f"Bearer {token}"}, )
         assert response.status_code == status.HTTP_404_NOT_FOUND, response.text
@@ -166,6 +183,9 @@ def test_update_contact_not_found(client, token):
 def test_delete_contact(client, token):
     with patch.object(auth_service, 'r') as r_mock:
         r_mock.get.return_value = None
+        tmp_day = datetime.now() + timedelta(days=3)  # Current date + 3 days
+        birthday = tmp_day.replace(year=1990)  # Change the year to 1990
+        birthday_str = birthday.strftime("%Y-%m-%d")
         response = client.delete(
             "/api/contacts/1",
             headers={"Authorization": f"Bearer {token}"}
@@ -176,7 +196,7 @@ def test_delete_contact(client, token):
         assert data["last_name"] == "Doe"
         assert data["email"] == "mike.doe@example.com"
         assert data["phone_number"] == "380504238517"
-        assert data["birthday"] == "1999-12-31"
+        assert data["birthday"] == birthday_str
         assert data["additional_info"] == "Update test"
         assert "id" in data
 
